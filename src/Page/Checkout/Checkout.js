ddb6fe1fd5af171eeeb934c4009fac6d6740a674
eshanHaque <eshan.haque16@gmail.com>
2022-04-18 23:44:23 +0600

    edit checkout component

--- src/Page/Checkout/Checkout.js
+++ src/Page/Checkout/Checkout.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import '../Checkout/Checkout.css'
 
 const Checkout = () => {
     return (
         <div>
-            <h2>checkout</h2>
+            <div className='checkout'>
+            <input type="text" name="name" id="" placeholder='Name' />
+            <input type="email" name="" id="" placeholder='email'/>
+            <input type="password" name="" id="" placeholder='password'/>
+            </div>
         </div>
     );
 };
