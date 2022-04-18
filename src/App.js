67e7beae0fb74d2c5a6f5afa3533632fd8af269a
eshanHaque <eshan.haque16@gmail.com>
2022-04-18 21:23:18 +0600

    require route set up done

--- src/App.js
+++ src/App.js
@@ -15,7 +15,11 @@ function App() {
   return (
     <div>
         <Routes>
-          <Route path="/" element={<Home></Home>}></Route>
+          <Route path="/" element={
+            <RequireAuth>
+              <Home></Home>
+            </RequireAuth>
+          }></Route>
           <Route path="/home" element={<Home></Home>}></Route>
           <Route path="/about" element={<About></About>}></Route>
           <Route path="/blog" element={<Blog></Blog>}></Route>
--- src/Page/Shared/Header/Header.js
+++ src/Page/Shared/Header/Header.js
@@ -1,8 +1,17 @@
+import { signOut } from 'firebase/auth';
 import React from 'react';
 import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link } from 'react-router-dom';
+import auth from '../../../firebase.init';
 import '../Header/Header.css'
+
 const Header = () => {
+    const [user] = useAuthState(auth);
+
+    const handleLogOut = () =>{
+        signOut(auth);
+    }
     return (
         <div>
             <header>
@@ -18,7 +27,12 @@ const Header = () => {
                             </Nav>
                             <Nav>
                                 <Nav.Link as={Link} to="about">About</Nav.Link>
-                                <Nav.Link as={Link} to="login">Login</Nav.Link>
+                                {
+                                    user ? 
+                                        <button onClick={handleLogOut}>Log out</button>
+                                    :
+                                    <Nav.Link as={Link} to="login">Login</Nav.Link>
+                                }
                             </Nav>
                         </Navbar.Collapse>
                     </Container>
